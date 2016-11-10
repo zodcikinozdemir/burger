27c9c3ee6fe47ce77cfa37880d552d2a2337baf7
zodcikin <zeynep.odcikin@gmail.com>
2016-11-10 00:54:45 -0500

    JAWDB

--- config/connection.js
+++ config/connection.js
@@ -1,18 +1,23 @@
 // Inside the connection.js file, setup the code to connect Node to MySQL.
 
 var mysql = require('mysql');
-var connection = mysql.createConnection({
-  host     : 'burgerdbinstance.c3xppfm7g1uf.us-east-1.rds.amazonaws.com',
-  user     : 'root',
-  password : 'root2016',
-  database : 'burgers_db'
-});
 
-connection.connect(function(err) {
-  if (err) {
-    console.error('error connecting: ' + err.stack);
-    return;
-  };
+var connection;
+	if(process.env.JAWSDB_URL) { 
+		mysql.createConnection(process.env.JAWSDB_URL);
+	} else {
+ 	connection = mysql.createConnection({
+  		host     : 'localhost',
+  		user     : 'root',
+  		password : 'root',
+  		database : 'burgers_db'
+	});
+
+	connection.connect(function(err) {
+  		if (err) {
+    		console.error('error connecting: ' + err.stack);
+    	return;
+  	};
 
 });
 
